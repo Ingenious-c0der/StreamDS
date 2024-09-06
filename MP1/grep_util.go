edec5334a8c9c41ab24d0dfa9197eb994979ab35
Ingenious-c0der <sagarabhyankar18@gmail.com>
2024-09-06 01:34:15 -0500

    -- almost everything worky

--- MP1/grep_util.go
+++ MP1/grep_util.go
@@ -1,20 +1,18 @@
-package main
+// package main
 
-import (
-	"fmt"
-	"os/exec"
-)
+// import (
+// 	"fmt"
+// 	"os/exec"
+// )
 
-func grepMain() {
-	// filepath to find if file present
-	file_name := "../todos.txt"
-	pattern := "grep -n 'query patterns' " + file_name
-	cmd := exec.Command("bash", "-c", pattern)
-	op, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println("Error ->", err)
-		// fmt.Println(string(op))
-	}
-	fmt.Println(string(op))
-	//
-}
+// func GrepMain(machine_file_name string, pattern string)   string {
+	
+// 	pattern = pattern + machine_file_name
+// 	cmd := exec.Command("bash", "-c", pattern)
+// 	op, err := cmd.CombinedOutput()
+// 	if err != nil {
+// 		fmt.Println("Error in grep util ->", err)
+// 	}
+// 	//convert bytes to string 
+// 	return string(op)
+// }
